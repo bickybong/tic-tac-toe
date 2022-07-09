e64370a5cd2f873f77941b5fafc1a78f98e9cae8
bicky <bickybong@gmail.com>
2022-07-09 10:45:18 +0800

    add click functions

--- script.js
+++ script.js
@@ -1,32 +1,49 @@
-var board = (function(){
-    gameboard = {top:[0,0,0], mid:[0,0,0], bot:[0,0,0]};
+var board = (function(){//board updates
+    const o = 'O';
+    const x = 'X';
+    const s = '';
+    gameboard = {top:[s,s,s], mid:[s,s,s], bot:[s,s,s]};
     const _body = document.querySelector('#mid');
     function viewboard(){for (const section in gameboard){
         count = 0;
-        // const container = document.createElement('div')
-        // container.classList.add(section);
-        // _body.appendChild(container);
         for (const char of gameboard[section]){
             const item = document.createElement('div')
-            item.classList.add(section + count);
+            item.classList.add('item');
             _body.appendChild(item);
             item.textContent = char;
+
             count++
-        }}}
+        }}};
     return {gameboard, viewboard};
 })();
 
 board.viewboard();
 
-var admin = (function(p1,p2){
-    const board1 = board
-    return board1;
-}())
-
-const players = (type) =>{
+const players = (type) =>{//player functions
     score = 0;
-    return {score, type};
+    const turn=()=>{
+        var children = document.querySelectorAll(".item");
+        children.forEach(div =>{
+            div.addEventListener('click', (e) => {
+                if (!e.target.textContent){
+                    e.target.style.backgroundColor = "azure";
+                    e.target.textContent = type;
+                }})
+        });
+        };
+    return {score, type, turn};
 }
 
-const p1 = players('X')
-const p2 = players('O')
+const p1 = players('X');
+const p2 = players('O');
+
+var admin = (function(){//turn order
+    console.log("turn")
+    const board1 = board;
+    while (true){
+        p1.turn();
+        p2.turn();
+        break;
+    };
+    return board1;
+})();
